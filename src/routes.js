fc994cbfbb5a643a4c22dd0969ae16b8665006aa
malves <malves@verios.com>
2020-09-10 11:36:50 -0300

    remove duplicated route

--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,6 @@ routes.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
-routes.get('/', (req, res) => {
-  res.json({ go: 'meli 💪💪' });
-});
-
 routes.get('/coverage', (req, res) => res.sendFile(resolve(__dirname, '..', 'coverage', 'index.html')));
 
 routes.post('/simian', SimianController.processDna);
